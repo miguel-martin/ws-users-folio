8491ba3c63367c355ee8dc6915a3cc2938873286
Miguel Martín <miguel@MacBook-Pro-de-Miguel.local>
2020-11-27 07:45:58 +0100

    Refactor api, add error handling

--- src/api/identidadAPI.js
+++ src/api/identidadAPI.js
@@ -6,18 +6,29 @@ export function IdentidadAPI(user, password) {
     this.WS_PWD = password
 
 
-    // Methods...
+    /**
+     * Get User List
+     */
     this.getUsers = async function () {
         console.log('Fetching users from ' + this.WS_URL)
         return fetch(this.CORS_PROXY + this.WS_URL, 
               { method:'GET', 
                 headers: {'Authorization': 'Basic ' + btoa(`${this.WS_USER}:${this.WS_PWD}`)}
             })
-        .then(response => response.json())
-        .then(json => {
-            console.log(json) // dev
-            return json;
-        });
+        .then(response => {
+            console.log('Response', response) //dev
+            if (response.ok)
+                return response.json()
+            else {
+                console.error(`Something went bad... Identidad API response status: ${response.status}`)
+                return []
+            }  
+        }).catch(error => {
+            console.log(error);
+        })
+        /*.then(json => {
+            console.log(json)
+        });*/
     }
 
 }

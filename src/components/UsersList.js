9de47cdf2ed2582862ef126f771dab5af012800a
Miguel Martín <miguel@MacBook-Pro-de-Miguel.local>
2020-11-27 08:03:53 +0100

    Add a link to get user details...

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import { IdentidadAPI } from '../api/identidadAPI'
 import './UsersList.css'
 
 export class UsersList extends Component {
 
-    constructor() {
-      super();
-      this.state = { users: [] };
-    }
+    state = { users: [] }
   
     async componentDidMount() {
         // create new API object...
@@ -17,11 +15,17 @@ export class UsersList extends Component {
         // get users...
         const data = await api.getUsers();
         console.log('Got data: ', data)
-        this.setState({ users: data });
+        this.setState({ users: data })
     }
 
     renderUser = (user) => {
-        return(<li className='user' key={user.nip} >{user.nip} - {user.nombre}</li>)
+        return(<li className='user' key={user.nip} >
+                <Link className='user-detail-link'
+                      to={`/users/${user.nip}`}>
+                          {user.nip}
+                </Link>
+                - {user.nombre}
+              </li>)
     }
 
     renderUsers = () => {

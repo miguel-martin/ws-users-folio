c359920bc788f48857fbea3080cfcc20091f969a
Miguel Martín <miguel@MacBook-Pro-de-Miguel.local>
2020-12-16 18:48:45 +0100

    DatePicker must be always rendered

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -3,24 +3,51 @@ import { IdentidadAPI } from '../api/identidadAPI'
 import { UserList } from './UserView'
 import { UZDatePicker } from './UZDatePicker'
 
+const DatePickerWrapper = ({since, dateChangeHandler}) => (
+  <div className="datepicker">
+    <span className="datepicker__text">Updated since: </span>
+    <span className="datepicker__picker">
+      <UZDatePicker sinceDate={since} handleDateChange={dateChangeHandler}/>
+    </span>
+   </div>
+)
+
 export class UsersList extends Component {
 
     state = { 
               api: new IdentidadAPI(process.env.REACT_APP_IDENTIDAD_API_USERNAME,
                                      process.env.REACT_APP_IDENTIDAD_API_PWD),
               message: 'Waiting for users API...',
-              since: new Date(2020, 0, 1), // by default, list users from 1 Jan 2020
+              since: new Date(2020, 11, 1), // by default, list users from 1 Dec 2020
               users: []
             }
 
     async listUsers(since) {
+
+        let message, users
+
         // get users...
         console.log('Listing users since ', since)
         const data = await this.state.api.getUsers(since);
         console.log('Got data: ', data)
+        // data can be undefined (server did not respond ok)
+        
+        
+        if (typeof data === 'object' && Array.isArray(data) && data.length > 0){ // an array with some items
+          message = `Results: ${data.length}`
+          users = data
+        }
+        else if (typeof data === 'object' && Array.isArray(data) && data.length === 0){ // an empty array
+          message = '0 results'
+          users = []
+        }
+        else { //(typeof data === 'undefined') {
+          message = 'API ERROR (check console)'
+          users = []
+        }
         this.setState({ 
-          message: (typeof data === 'undefined') ? 'API ERROR (check console)' : '',
-          users: (typeof data !== 'undefined') ? data : []
+          message: message,
+          users: users
         })
     }
   
@@ -34,20 +61,19 @@ export class UsersList extends Component {
       this.setState({ since: date, users: [], message: 'New date. Waiting for users API...' })
       this.listUsers(date)
     }
+
+
   
     render() {
       console.log(this.state) // dev
       return (
         <div>
+            <DatePickerWrapper since={this.state.since} dateChangeHandler={this.handleDateChange}/>
             { !this.state.users.length
-              ? this.state.message
-              : [ <div className="datepicker">
-                    <span className="datepicker__text">Updated since: </span>
-                    <span className="datepicker__picker">
-                      <UZDatePicker sinceDate={this.state.since} handleDateChange={this.handleDateChange}/>
-                    </span>
-                  </div>,
-                  <UserList users={this.state.users} 
+              ? <div className='message'>
+                  {this.state.message}
+                </div>
+              : [ <UserList users={this.state.users} 
                             year={this.state.since.getFullYear()}
                             month={this.state.since.getMonth()+1}
                             day={this.state.since.getDate()} />
